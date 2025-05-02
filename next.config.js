1de50c4b246f68c5cf9fca415e4240c490ce8690
Khalfan AlKindi <khalfan500@hotmail.com>
2025-05-02 13:31:42 +0400

    update the urls

--- next.config.js
+++ next.config.js
@@ -9,18 +9,25 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            //value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self' ws:; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; frame-src 'self'"
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval' 'inline-speculation-rules';
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data:;
-              font-src 'self' data:;
-              connect-src 'self' https://dararabappbackendv01-production.up.railway.app ws:;
-              frame-src 'self'
-            `.replace(/\n/g, ""),
-          }
-        ]
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval' 'inline-speculation-rules'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self' https://dararabappbackendv01-production.up.railway.app; frame-src 'self';",
+          },
+        ],
+        // headers: [
+        //   {
+        //     key: 'Content-Security-Policy',
+        //     //value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self' ws:; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; frame-src 'self'"
+        //     value: `
+        //       default-src 'self';
+        //       script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval' 'inline-speculation-rules';
+        //       style-src 'self' 'unsafe-inline';
+        //       img-src 'self' data:;
+        //       font-src 'self' data:;
+        //       connect-src 'self' https://dararabappbackendv01-production.up.railway.app ws:;
+        //       frame-src 'self'
+        //     `.replace(/\n/g, ""),
+        //   }
+        // ]
       }
     ]
   },
