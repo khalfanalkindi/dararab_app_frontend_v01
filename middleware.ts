5a20038e8a94b3636f184d84c0c336b1098b6d5a
Khalfan AlKindi <khalfan500@hotmail.com>
2025-05-02 15:14:50 +0400

    Update CSP to production backend only

--- middleware.ts
+++ middleware.ts
@@ -7,11 +7,11 @@ export function middleware(request: NextRequest) {
   // Set CSP headers with a more permissive configuration
   response.headers.set(
     'Content-Security-Policy',
-    "default-src 'self' http://127.0.0.1:8000; " +
-    "connect-src 'self' http://127.0.0.1:8000; " +
-    "img-src 'self' data: http://127.0.0.1:8000; " +
-    "script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval' 'inline-speculation-rules' chrome-extension://*; " +
-    "style-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+    "default-src 'self'; " +
+    "connect-src 'self' https://dararabappbackendv01-production.up.railway.app; " +
+    "img-src 'self' data:; " +
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval' 'inline-speculation-rules'; " +
+    "style-src 'self' 'unsafe-inline'; " +
     "font-src 'self' data:; " +
     "frame-src 'self'; " +
     "object-src 'none'; " +
